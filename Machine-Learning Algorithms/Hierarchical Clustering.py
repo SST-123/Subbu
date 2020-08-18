77c178af7b89895a0fa15f8932462c0c8c67dcc3
SST\Profession <56752191+SST-123@users.noreply.github.com>
2020-08-18 11:32:25 +0530

    Add files via upload

---
+++ Machine-Learning Algorithms/Hierarchical Clustering.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 11:06:09 2020
+
+@author: admin
+"""
+#importing the libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#importing the mall dataset with pandas
+dataset = pd.read_csv("Mall_Customers.csv")
+dataset.head()
+X = dataset.iloc[:, [3,4]].values
+
+#Using the Dendrogram to find the optimum clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Eucledian Distance')
+plt.show()
+
+#Fitting Hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+#Visualizing the clusters
+
+plt.figure(figsize=(6,4))
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c='red',label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c='blue',label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c='green',label = 'Target')    
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c='cyan',label = 'Careless')    
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c='magenta',label = 'Sensible')    
+plt.title('Clusters of  clients')
+plt.xlabel('Annual Income(k$)')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
